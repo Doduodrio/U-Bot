e15c9437f28d546445f1f82b102343588241ad14
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2025-06-13 19:06:29 -0700

    Implement basic mirroring

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from discord import app_commands
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 
+CHANNELS = [
+    1143723280176525325,
+    1383253050776485981
+]
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -31,5 +36,27 @@ async def on_ready():
     
     await client.get_user(587040390603866122).send('U-Bot is online')
     print('U-Bot sent a DM to doduodrio upon activating!')
+
+@client.event
+async def on_message(message: discord.Message):
+    
+    # don't respond to own messages
+    if message.author == client.user:
+        return
+    
+    # don't broadcast messages outside of channels list
+    if message.channel.id not in CHANNELS:
+        return
+    
+    # message.author, message.guild, message.content, message.embeds, message.channel
+    # message.reference (replies), message.attachments
+
+    for id in [id for id in CHANNELS if id != message.channel.id]:
+        await client.get_channel(id).send(
+            content=message.content,
+            embeds=message.embeds,
+            files=[await a.to_file(filename=a.filename, spoiler=a.is_spoiler()) for a in message.attachments],
+            stickers=message.stickers
+        )
 
 client.run(TOKEN)
